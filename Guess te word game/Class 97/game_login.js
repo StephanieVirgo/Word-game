8c7a1b6a4122b9697356cba4a91a391d53df1742
StephanieVirgo <126833440+StephanieVirgo@users.noreply.github.com>
2023-06-18 18:26:32 -0400

    Add files via upload

---
+++ Guess te word game/Class 97/game_login.js
@@ -0,0 +1,9 @@
+function addUser() {
+    playe1_name = document.getElementById("player1_name_input").ariaValueMax;
+    playe2_name = document.getElementById("player2_name_input").ariaValueMax;
+
+      localStorage.setItem("player1_name", player1_name);
+      localStorage.setItem("player2_name", player2_name);
+
+      window.location = "game_page.html";
+}
